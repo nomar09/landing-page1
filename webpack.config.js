bda7189e3e8c1523fd12bd2e73ceaffe4698212a
Nomar <nomarlonon1@gmail.com>
2021-05-22 00:41:36 +0800

    finish header and heroshot and update the whole production automation and ready to create any website

--- webpack.config.js
+++ webpack.config.js
@@ -1,105 +1,110 @@
-const currentTask = process.env.npm_lifecycle_event
-const path = require('path')
-const {CleanWebpackPlugin} = require ('clean-webpack-plugin')
-const MiniCssExtractPlugin = require ('mini-css-extract-plugin')
-const CssMinimizerPlugin = require ('css-minimizer-webpack-plugin')
-const HtmlWebpackPlugin = require ('html-webpack-plugin')
-const fse = require('fs-extra')
+const currentTask = process.env.npm_lifecycle_event;
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const fse = require("fs-extra");
 
 const postCSSPlugins = [
-    require('postcss-import'),
-    require('postcss-mixins'),
-    require('postcss-simple-vars'),
-    require('postcss-nested'),
-    require('postcss-hexrgba'),
-    require('autoprefixer')
-]
+  require("postcss-import"),
+  require("postcss-mixins"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("postcss-hexrgba"),
+  require("autoprefixer"),
+];
 
 class RunAfterCompile {
-    apply(compiler){
-        compiler.hooks.done.tap('Copy images', function(){
-            fse.copySync('./app/assets/images', './docs/assets/images')   
-        })
-    }
+  apply(compiler) {
+    compiler.hooks.done.tap("Copy images", function () {
+      fse.copySync("./app/assets/images", "./docs/assets/images");
+    });
+  }
 }
 
 let cssConfig = {
-    test: /\.css$/i,
-    use: ['css-loader?url=false', {loader: 'postcss-loader', options: {postcssOptions: {plugins: postCSSPlugins}}}]
-}
+  test: /\.css$/i,
+  use: [
+    "css-loader?url=false",
+    {
+      loader: "postcss-loader",
+      options: { postcssOptions: { plugins: postCSSPlugins } },
+    },
+  ],
+};
 
-let pages = fse.readdirSync('./app').filter(function(file) {
-    return file.endsWith('.html')
-}).map(function(page){
+let pages = fse
+  .readdirSync("./app")
+  .filter(function (file) {
+    return file.endsWith(".html");
+  })
+  .map(function (page) {
     return new HtmlWebpackPlugin({
-        filename: page,
-        template: `./app/${page}`
-    })
-})
+      filename: page,
+      template: `./app/${page}`,
+    });
+  });
 
 let config = {
-    entry: './app/assets/scripts/App.js',
-    plugins: pages,
-    module: {
-        rules:[
-            cssConfig
-        ]
-    }
+  entry: "./app/assets/scripts/App.js",
+  plugins: pages,
+  module: {
+    rules: [cssConfig],
+  },
+};
+
+if (currentTask == "dev") {
+  cssConfig.use.unshift("style-loader");
+  config.output = {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "app"),
+  };
+
+  config.devServer = {
+    before: function (app, server) {
+      server._watch("./app/**/*.html");
+    },
+    contentBase: path.join(__dirname, "app"),
+    hot: true,
+    port: 3005,
+    host: "0.0.0.0",
+  };
+
+  config.mode = "development";
 }
 
-if (currentTask == 'dev') {
-    cssConfig.use.unshift('style-loader')
-    config.output = {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'app')
-    }
+if (currentTask == "build") {
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+      },
+    },
+  });
 
-    config.devServer = {
-        before: function (app, server) {
-            server._watch('./app/**/*.html')
-        },
-        contentBase: path.join(__dirname, 'app'),
-        hot: true,
-        port:3000,
-        host: '0.0.0.0'
-    }
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader);
+  config.output = {
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "docs"),
+  };
 
-    config.mode = 'development'
+  config.mode = "production";
+  config.optimization = {
+    splitChunks: { chunks: "all", minSize: 1000 },
+    minimize: true,
+    minimizer: [`...`, new CssMinimizerPlugin()],
+  };
+
+  config.plugins.push(
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "styles.[chunkhash].css" }),
+    new RunAfterCompile()
+  );
 }
 
-if (currentTask == 'build') {
-    config.module.rules.push({
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env']
-            }
-        }
-    })
-
-    cssConfig.use.unshift(MiniCssExtractPlugin.loader)
-    config.output = {
-        filename: '[name].[chunkhash].js',
-        chunkFilename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'docs')
-    }
-
-    config.mode = 'production'
-    config.optimization = {
-        splitChunks: {chunks: 'all', minSize: 1000},
-        minimize: true,
-        minimizer: [`...`, new CssMinimizerPlugin()]
-    }
-
-    config.plugins.push(
-        new CleanWebpackPlugin(), 
-        new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
-        new RunAfterCompile()
-        )
-}
-
-
-
-module.exports = config
+module.exports = config;
