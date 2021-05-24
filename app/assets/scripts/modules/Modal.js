e2ed8665b8c0cba8ae2a7a49a0bfb5d7a112e3d3
Nomar <nomarlonon1@gmail.com>
2021-05-25 00:38:49 +0800

    just ignore this

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -1,41 +1,37 @@
 class Modal {
+  constructor() {
+    this.injectHTML();
+    this.modal = document.querySelector(".modal");
+    this.closeIcon = document.querySelector(".modal__close");
+    this.events();
+  }
 
-    constructor(){
-        this.injectHTML()
-        this.modal = document.querySelector(".modal")
-        this.closeIcon = document.querySelector(".modal__close")
-        this.events()
+  events() {
+    // listen for close click
+    this.closeIcon.addEventListener("click", () => this.closeTheModal());
+
+    // pushes any key
+    document.addEventListener("keyup", (e) => this.keyPressHandler(e));
+  }
+
+  keyPressHandler(e) {
+    if (e.keyCode == 27) {
+      this.closeTheModal();
     }
+  }
 
-    events(){
-       
+  openTheModal() {
+    this.modal.classList.add("modal--is-visible");
+  }
 
-        // listen for close click
-        this.closeIcon.addEventListener("click", () => this.closeTheModal())
+  closeTheModal() {
+    this.modal.classList.remove("modal--is-visible");
+  }
 
-        // pushes any key
-        document.addEventListener("keyup", e => this.keyPressHandler(e))
-    }
-
-    keyPressHandler(e){
-        if(e.keyCode == 27){
-            this.closeTheModal()
-        }
-    }
-
-    openTheModal(){
-        
-        this.modal.classList.add("modal--is-visible")
-    }
-
-
-    closeTheModal(){
-        this.modal.classList.remove("modal--is-visible")
-    }
-
-
-    injectHTML(){
-        document.body.insertAdjacentHTML('beforeend', `
+  injectHTML() {
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      `
         <div class="modal">
             <div class="modal__inner">
             <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg" class="section-title__icon"> Get in <strong>Touch</strong></h2>
@@ -52,9 +48,9 @@ class Modal {
             </div>
             <div class="modal__close">X</div>
         </div>
-        `)
-    }
-
+        `
+    );
+  }
 }
 
-export default Modal
+export default Modal;
